619eaf0853861aebecd751499196548adc09995f
Elev <Elev@TEB123008>
2019-09-06 10:15:32 +0200

    Lagt till projektet

---
+++ Inlämningsuppgift/Inlämningsuppgift/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Inlämningsuppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random Random = new Random();
+            int RandomNumber = Random.Next(1, 100);
+            Console.WriteLine("Skriv ett tal mellan 1 och 100: ");
+            int tal = 0;
+            while (tal = Random)
+            if (tal == Random)
+                WriteLine("Du har fått rätt tal")
+            else if (tal =! Random)
+                WriteLine("Försök igen")
+            
+            
+
+
+            
+        }
+    }
+}
